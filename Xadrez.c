3d6e8f0d3ebec9a5533f6a42b5c2ce233814efff
johnnywow17-oss <johnnywow17@gmail.com>
2026-05-18 20:49:34 +0000

    Xadrez como void

---
+++ Xadrez.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void movertorrer(int casas ){
+    if (casas >0){
+        printf("Direita\n");
+        movertorrer(casas -1);
+    }
+}
+void moverbispo(casas){
+    if (casas > ){
+        for (int vertical = 0; vertical < 1; vertical ++){
+            for(int horizontal = 0, horizontal < 1; horizontal ++){
+                printf("Cima\nDireita\n ");
+            }
+        }
+        
+    }
+
+}
